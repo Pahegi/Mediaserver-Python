641e338254f5890804938d27047529abf237d16e
Pahegi <pahegi@gmx.de>
2021-07-10 21:12:00 +0200

    Updated Updateskript

--- updatejekyll.sh
+++ updatejekyll.sh
@@ -7,7 +7,6 @@ killall python3
 rm /home/pi/Mediaserver-Python/mainjekyll.py
 
 curl -LJO https://raw.githubusercontent.com/Pahegi/Mediaserver-Python/master/mainjekyll.py
-curl -LJO https://raw.githubusercontent.com/Pahegi/Mediaserver-Python/master/mainrhs.py
 
 python3 /home/pi/Mediaserver-Python/mainjekyll.py
 
