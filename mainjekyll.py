121966a3e98aa5df9213a1bcabfa67614a3c6ca0
Pahegi <pahegi@gmx.de>
2021-07-11 00:50:35 +0200

    Refactored Code

--- mainjekyll.py
+++ mainjekyll.py
@@ -6,80 +6,157 @@ import sacn
 import time
 import os
 
-##############################################################
-# DMX Footprint:
-# CH1: Selection of Video (0 Stop, 1-255 Play Files)
-# CH2: Selection of Folder (0-255)
-# CH3: Selection of Playmode (0-127 Non-Loop, 128-255 Loop)
-##############################################################
-
-
+################
+# SERVER-CLASS #
+################
 class Server:
-  #Constructor
   def __init__ (self):
-    self.CH1Current = 0                                 
-    self.CH1Last = 0                                    
-    self.CH2Current = 0                                 
-    self.CH2Last = 0                                
-    self.address = 1                                    #DMX Start Address
-    self.universe = 1                                   #DMX Universe
-    self.receiver = sacn.sACNreceiver()                 #sACN Receiver
-    self.data = 0;
-    mediapath = "/home/pi/media/"                       #Path of Media Folders and Files
-    try:
-      config = configparser.ConfigParser()
-      config.read("/home/pi/config.txt")
-      self.address = config.getint("DMX-Konfiguration", "Adresse")
-      self.universe = config.getint("DMX-Konfiguration", "Universum")
-      print("Loaded adress", self.address, "and universe", self.universe, " from configfile")
-    except Exception as e:
-      print(traceback.format_exc())
-      print("Couldn't find config.txt, loaded adress", self.address, "and universe", self.universe)
+    self.mediapath = "/home/pi/media/"
+    self.configpath = "/home/pi/config.txt"
+    self.dmx = DMX()
+    self.vlc = VLC()
+    self.channellist = Channellist(self.dmx.address)
+    self.receiver = sacn.sACNreceiver()
+    self.loadConfig(self.configpath)
 
-    self.vlc_instance = vlc.Instance()                  #VLC Instance
-    self.player = self.vlc_instance.media_player_new()
-
-
-    #Define DMX Packet Callback
-    @self.receiver.listen_on('universe', universe=self.universe)
+  #start()
+  #starts Mediaserver
+  def start(self):
+    #DMX Packet Callback
+    @self.receiver.listen_on('universe', universe=self.dmx.universe)
     def callback(packet):
-      self.data = packet.dmxData
-
-      #Selection of Video
-      self.CH1Current = self.data[self.address-1]
-      self.CH2Current = self.data[self.address]
-      if ((self.CH1Current != self.CH1Last) or (self.CH2Current != self.CH2Last)): #When values change
-        if (self.CH1Current > 0):
-          try:
-            #Sorting and selection of folders/files
-            folderlist = sorted(os.listdir(mediapath))
-            if (len(folderlist) >= self.CH2Current):
-              folder = folderlist[self.CH2Current]
-              filelist = sorted(os.listdir(mediapath + "/" + folder))
-              if (len(filelist) >= self.CH1Current):
-                file = filelist[self.CH1Current-1]
-                playpath = (mediapath + folder + "/" + file)
-                #Wiedergabe
-                self.media = self.vlc_instance.media_new(playpath)
-                self.player.set_media(self.media)
-                print("Playing new Media with Loop " + ("on: " if (self.data[self.address+1] > 127) else "off: ") + playpath)
-                self.media.add_option("input-repeat=" + str(10000 if (self.data[self.address+1] > 127) else 0))
-                self.player.play()
-          except Exception as e:
-            print(traceback.format_exc())
+      self.channellist.update(packet)
+      if (self.channellist.isNew(0) | self.channellist.isNew(1)):
+        if (self.channellist.get(0) > 0):
+          playpath = self.getPlaypath(self.mediapath, self.channellist)
+          if (playpath != ""):
+            self.vlc.setMedia(playpath)
+            self.vlc.setLoop(self.channellist.get(2) > 127)
+            self.vlc.play()
+          else:
+            self.vlc.stop()
         else:
-          print("Stopping Player")
-          self.player.stop()
-          
-      self.CH1Last = self.CH1Current
-      self.CH2Last = self.CH2Current
-    self.receiver.start()  # start the receiving thread
+          self.vlc.stop()
+    self.receiver.start()
     self.receiver.join_multicast(1)
 
+  #loadConfig()
+  #loads configfile and sets dmx values
+  def loadConfig(self, configpath):
+    try:
+      config = configparser.ConfigParser()
+      config.read(configpath)
+      self.dmx.address = config.getint("DMX-Konfiguration", "Adresse")
+      self.dmx.universe = config.getint("DMX-Konfiguration", "Universum")
+      print("Loaded adress", self.dmx.toString(), "from configfile")
+      return
+    except Exception as e:
+      print(traceback.format_exc())
+      print("Couldn't find config.txt, loaded adress", self.dmx.toString())
+      return
 
+  #getPlaypath(mediapath, channellist)
+  #calculates path of media file based on base-path and DMX-values
+  def getPlaypath(self, mediapath, channellist):
+    folderlist = sorted(os.listdir(mediapath))
+    if (len(folderlist)-1 >= self.channellist.get(1)):
+      folder = folderlist[self.channellist.get(1)]
+      filelist = sorted(os.listdir(mediapath + "/" + folder))
+      if (len(filelist) >= self.channellist.get(0)):
+        file = filelist[self.channellist.get(0)-1]
+        playpath = (mediapath + folder + "/" + file)
+        return playpath
+    return ""
+
+#################
+# CHANNEL-CLASS #
+#################
+class Channel:
+  def __init__ (self, address):
+    self.current = -1
+    self.last = -1
+    self.new = True
+    self.address = address
+
+  def update(self, packet):
+    value = packet.dmxData[self.address - 1]
+    if (self.current != value):
+      self.last = self.current
+      self.current = value
+      self.new = True
+    else:
+      self.new = False
+    return
+
+  def isNew(self):
+    return self.new
+
+#####################
+# CHANNELLIST-CLASS #
+#####################
+class Channellist:
+  def __init__ (self, address):
+    self.channel = [Channel(i+address) for i in range(3)]
+
+  def update(self, packet):
+    for i in range(3):
+      self.channel[i].update(packet)
+
+  def isNew(self, offset):
+    return self.channel[offset].isNew()
+
+  def get(self, offset):
+    return self.channel[offset].current
+
+#############
+# DMX-CLASS #
+#############
+class DMX:
+  def __init__(self):
+    self.address = 1
+    self.universe = 1
+  
+  def toString(self):
+    return (str(self.universe) + "." + str(self.address))
+
+#############
+# VLC-CLASS #
+#############
+class VLC:
+  def __init__(self):
+    self.vlc_instance = vlc.Instance()
+    self.player = self.vlc_instance.media_player_new()
+    self.loop = False
+    self.playpath = ""
+
+  def setMedia(self, playpath):
+    try:
+      if (playpath != ""):
+        self.media = self.vlc_instance.media_new(playpath)
+        self.player.set_media(self.media)
+        self.playpath = playpath
+    except Exception as e:
+            print(traceback.format_exc())
+
+  def setLoop(self, loop):
+    if (self.playpath != ""):
+      self.media.add_option("input-repeat=" + str(10000 if loop else 0))
+    
+  def play(self):
+    if (self.playpath != ""):
+      print("Starting Media '" + self.playpath + "' with Loop", "on" if self.loop else "off")
+      self.player.play()
+
+  def stop(self):
+    print("Stopping Media")
+    self.player.stop()
+
+###############
+# MAIN-METHOD #
+###############
 def main():
-  os.system("tvservice -p")
-  server = Server()
+  mediaserver = Server()
+  mediaserver.start()
 
 if __name__ == '__main__':
   main()
